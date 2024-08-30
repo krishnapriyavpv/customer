a008caf7b30f83bc0d54c6ff77860f2024cef6b2
krishnapriyavpv <krishnapriyavpv@gmail.com>
2024-08-30 07:08:16 +0530

    cust-new1

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,10 @@
 import 'package:customer/home_nav_page.dart';
-import 'package:customer/vendor/screens/auth/login_page.dart';
-import 'package:customer/vendor/utils/constants.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  await Firebase.initializeApp();
   runApp(const MainApp());
 }
 
@@ -19,17 +13,8 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        routes: {
-          '/login': (context) => const LogInPage(),
-          // other routes
-        },
-        theme: ThemeData.dark().copyWith(
-          scaffoldBackgroundColor: bgColor,
-          textTheme: GoogleFonts.poppinsTextTheme(Theme.of(context).textTheme)
-              .apply(bodyColor: Colors.white),
-          canvasColor: secondaryColor,
-        ),
-        home: const HomeNavPage());
+    return const MaterialApp(home: HomeNavPage());
   }
 }
+
+
